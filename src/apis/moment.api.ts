e4e246eec708c51c5809821b12cbf833754ddf70
koyrkr <koyrkr@gmail.com>
2023-07-01 17:16:52 +0900

    fix: fix updated moment api & api instance config

--- src/apis/moment.api.ts
+++ src/apis/moment.api.ts
@@ -1,10 +1,51 @@
 import { MomentType } from '@types';
 import API from './API';
+import { tsUtils } from '@utils';
 
-// moment 업로드
-export const uploadMoment = (params: MomentType.MomentData) =>
-  API.post('/moment/today', params);
+// GET today's moment
+export const getTodayMoment = async () => {
+  const { year, month, day } = getMomentRequestParams(new Date());
+  const res = await API.get<MomentType.TodayMoment>(
+    `/moment/daily/${year}/${month}/${day}/`,
+  );
+  return res;
+};
 
-// moment 수정
-export const updateMoment = (params: MomentType.MomentData) =>
-  API.put('/moment/today', params);
+// POST today's moment
+export const postTodayMoment = async (
+  moment: Partial<MomentType.TodayMoment>,
+) => {
+  const { year, month, day } = getMomentRequestParams(new Date());
+  const momentFormData = tsUtils.objectFormDataSerializer(moment);
+  const res = await API.post<MomentType.TodayMoment>(
+    `/moment/daily/${year}/${month}/${day}/`,
+    momentFormData,
+    {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    },
+  );
+  return res;
+};
+
+// PUT today's moment
+export const updateTodayMoment = async (
+  moment: Partial<MomentType.TodayMoment>,
+) => {
+  const { year, month, day } = getMomentRequestParams(new Date());
+  const momentFormData = tsUtils.objectFormDataSerializer(moment);
+  const res = await API.put<MomentType.TodayMoment>(
+    `/moment/daily/${year}/${month}/${day}/`,
+    momentFormData,
+    {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    },
+  );
+  return res;
+};
+
+export const getMomentRequestParams = (date: Date) => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return { year, month, day };
+};
