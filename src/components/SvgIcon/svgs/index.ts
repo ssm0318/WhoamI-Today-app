93733b1226174f552bb48cb0b12860464507c946
koyrkr <koyrkr@gmail.com>
2023-08-06 02:11:54 +0900

    fix: renewal photo upload screen UI

--- src/components/SvgIcon/svgs/index.ts
+++ src/components/SvgIcon/svgs/index.ts
@@ -3,6 +3,8 @@ import camera_flash_on from './camera_flash_on.svg';
 import camera_photo from './camera_photo.svg';
 import camera_switch from './camera_switch.svg';
 import moment_skip from './moment_skip.svg';
+import navigation_left_white from './navigation_left_white.svg';
+import close_white from './close_white.svg';
 
 export {
   camera_flash_off,
@@ -10,4 +12,6 @@ export {
   camera_photo,
   camera_switch,
   moment_skip,
+  navigation_left_white,
+  close_white,
 };
