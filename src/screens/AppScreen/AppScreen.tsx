0017a9b906e16f2205c5123a0d1735accbb7ebd4
Gina <koyrkr@gmail.com>
2023-08-19 09:57:27 +0900

    (#28) 웹뷰 내에서도 왼쪽으로 swipe시 이전 화면으로 이동 (#29)
    feat: add allowsBackForwardNavigationGestures

--- src/screens/AppScreen/AppScreen.tsx
+++ src/screens/AppScreen/AppScreen.tsx
@@ -40,8 +40,9 @@ const AppScreen: React.FC<AppScreenProps> = ({ route }) => {
         ref={ref}
         onMessage={onMessage}
         source={{
-          uri: WEBVIEW_CONSTS.WEB_VIEW_URL.DEV + url,
+          uri: WEBVIEW_CONSTS.WEB_VIEW_URL.PROD + url,
         }}
+        allowsBackForwardNavigationGestures
         decelerationRate="normal"
         javaScriptEnabled
         injectedJavaScript={WEBVIEW_CONSTS.WEB_VIEW_DEBUGGING_SCRIPT}
