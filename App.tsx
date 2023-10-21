a3b00c8b9d9686cc7431e3f498bc355a9da4d073
koyrkr <koyrkr@gmail.com>
2023-10-20 19:49:39 -0700

    test: test commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
-// import { RootNavigator } from '@navigation';
+import { RootNavigator } from '@navigation';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const App: React.FC = () => {
