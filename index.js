ebe6d1fffc2e8974de15d566b2ae052289011e95
Gina <koyrkr@gmail.com>
2024-07-13 14:15:55 +0900

    fix: fix index.js for background on IOS

--- index.js
+++ index.js
@@ -8,23 +8,19 @@ import { name as appName } from './app.json';
 import 'intl-pluralrules';
 import './src/i18n';
 import messaging from '@react-native-firebase/messaging';
-import notifee, { EventType } from '@notifee/react-native';
-import NavigationService from './src/libs/NavigationService';
 
 // Background handling with Firebase messaging
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-  //  background 에 대한 실제 로직은 notifee 로 이용
   console.log('[Firebase remote message on background] : ', remoteMessage);
 });
 
-notifee.onBackgroundEvent(async ({ type, detail }) => {
-  console.log('[Notifee background event]: ', type, detail);
-  if (type === EventType.PRESS) {
-    const url = detail.notification?.data?.url;
-    if (typeof url === 'string') {
-      NavigationService.navigate('AppScreen', { url });
-    }
+function HeadlessCheck({ isHeadless }) {
+  if (isHeadless) {
+    // App has been launched in the background by iOS, ignore
+    return null;
   }
-});
 
-AppRegistry.registerComponent(appName, () => App);
+  return <App />;
+}
+
+AppRegistry.registerComponent(appName, () => HeadlessCheck);
